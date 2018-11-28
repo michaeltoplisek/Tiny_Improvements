4ff0fdc31f593d1c0fab53437060429e5df42cd1
Tops <mtoplisek95@gmail.com>
2018-11-27 22:31:26 -0500

    set up mlab

--- server.js
+++ server.js
@@ -9,9 +9,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+var databaseUri = 'mongodb://localhost/tinyimprovements';
 
-mongoose.connect('mongodb://localhost/tinyimprovements', { useNewUrlParser: true });
-
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI)
+} else {
+  mongoose.connect(databaseUri)
+}
 
 require('./routes.js')(app);
 
