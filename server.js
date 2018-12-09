e223b8e14f1ed92c8433beaf90b66f057ddfc97f
Tops <mtoplisek95@gmail.com>
2018-12-08 23:34:29 -0500

    yo

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build/index.html"));
+  app.use(express.static("client/public/index.html"));
 } else {
   app.use(express.static("./client/public"));
 }
